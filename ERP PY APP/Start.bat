c41d2a672a6ce81568207bee6c2f57fdbecfbb3a
Danger-Khan <imaadyameen125@gmail.com>
2026-08-03 22:26:55 +0500

    Delete ERP PY APP directory

--- ERP PY APP/Start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Change directory to project root folder
-cd /d "%~dp0"
-
-:: Start Flask server silently in the background
-start "" ".venv\Scripts\pythonw.exe" run.py
-
-:: Start the Desktop GUI
-start "" ".venv\Scripts\pythonw.exe" main_gui.py
